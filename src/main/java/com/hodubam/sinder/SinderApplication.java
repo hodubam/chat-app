7db52749af0a7523d0732a9f9d4d7ca5ce954d06
hodubam <chanhoehwang@gmail.com>
2024-04-13 00:16:17 +0900

    first commit

---
+++ src/main/java/com/hodubam/sinder/SinderApplication.java
@@ -0,0 +1,13 @@
+package com.hodubam.sinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.hodubam.sinder.SinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chanhodu/sindor/SindorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanhodu.sindor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SindorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

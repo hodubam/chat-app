e4a3eee6722e6b3001ed0ed11280c78abfb883d9
hodubam <chanhoehwang@gmail.com>
2024-04-26 01:57:59 +0900

    signup form data transaction commit

--- src/main/webapp/js/signUp.js
+++ src/main/webapp/js/signUp.js
@@ -1,5 +1,13 @@
 let email = document.querySelector('[name="email"]');
 let emailFailMessage = document.querySelector('#email_fail_message');
+let username = document.querySelector('[name="username"]');
+let usernameFailMessage = document.querySelector('#username_fail_message');
+let password = document.querySelector('[name="password"]');
+let passwordFailMessage = document.querySelector('#password_fail_message');
+let confirmPassword = document.querySelector('[name="confirm_password"]');
+let confirmPasswordFailMessage = document.querySelector('#confirm_password_fail_message');
+let signUpForm = document.querySelector('#sighUpForm');
+let submitButton = document.querySelector('#submit');
 
 email.onkeyup = function () {
     if(email.value.length !== 0){
@@ -14,10 +22,6 @@ email.onkeyup = function () {
     signUpCheck();
 }
 
-
-let username = document.querySelector('[name="username"]');
-let usernameFailMessage = document.querySelector('#username_fail_message');
-
 username.onkeyup = function () {
     if(username.value.length !== 0){
         if(/^[A-Za-z0-9][A-Za-z0-9]*$/.test(username.value) === false || (username.value.length > 12 || username.value.length < 4)) {
@@ -30,9 +34,6 @@ username.onkeyup = function () {
     }
     signUpCheck();
 }
-
-let password = document.querySelector('[name="password"]');
-let passwordFailMessage = document.querySelector('#password_fail_message');
 
 password.onkeyup = function () {
     if(password.value.length !== 0){
@@ -47,8 +48,6 @@ password.onkeyup = function () {
     signUpCheck();
 }
 
-let confirmPassword = document.querySelector('[name="confirm_password"]');
-let confirmPasswordFailMessage = document.querySelector('#confirm_password_fail_message');
 
 confirmPassword.onkeyup = function () {
     if(confirmPassword.value.length !== 0){
@@ -63,32 +62,57 @@ confirmPassword.onkeyup = function () {
     signUpCheck();
 }
 
-
-
-function signUpCheck(){
-    if(email.classList.contains('hide') &&
+function allPass() {
+    if(emailFailMessage.classList.contains('hide') &&
         usernameFailMessage.classList.contains('hide') &&
         passwordFailMessage.classList.contains('hide') &&
         confirmPasswordFailMessage.classList.contains('hide') &&
         email.value.length > 0 &&
-        usernameFailMessage.value.length > 0 &&
-        passwordFailMessage.value.length > 0 &&
-        confirmPasswordFailMessage.value.length > 0
-    ) document.querySelector('.submit').style.backgroundColor = '#4285F4'
-    else{
-        document.querySelector('.submit').style.backgroundColor = 'darkgray'
+        username.value.length > 0 &&
+        password.value.length > 0 &&
+        confirmPassword.value.length > 0
+    ) return true;
+    return false;
+}
+
+function signUpCheck(){
+    if(allPass()){
+        submitButton.style.backgroundColor = '#4285F4';
+    }else{
+        submitButton.style.backgroundColor = 'darkgray';
     }
 }
 
-function goToSignIn() {
+signUpForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if(allPass()){
+        let formData = {
+            "email":email.value
+            ,"username":username.value
+            ,"password":password.value
+        };
+
+        fetch('http://localhost:8080/signUp', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.text();
+                }
+                throw new Error('Network response was not ok.');
+            })
+            .then(data => {
+                console.log(data); // 서버 응답 출력
+                // 원하는 처리 수행
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
 
 
-
-
-
-
-}
-
-function goToSignUp() {
-    window.location.href="/pages/signUp.html";
-}
+})

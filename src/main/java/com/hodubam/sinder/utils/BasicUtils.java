777cb089839acafe187e734296171ee60ebda413
hodubam <chanhoehwang@gmail.com>
2024-05-12 01:55:56 +0900

    decrypt module added

--- src/main/java/com/hodubam/sinder/utils/BasicUtils.java
+++ src/main/java/com/hodubam/sinder/utils/BasicUtils.java
@@ -1,11 +1,19 @@
 package com.hodubam.sinder.utils;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import java.util.UUID;
 
 public class BasicUtils {
 
+    private static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public static String createUUID() {
         UUID uuid = UUID.randomUUID();
         return uuid.toString();
+    }
+
+    public static String encrypt(String str){
+        return encoder.encode(str);
     }
 }
